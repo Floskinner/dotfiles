f3312419c07132b0fb863a176f862f2af094bd9c
Florian Glaser <58706771+Floskinner@users.noreply.github.com>
2024-06-09 12:42:47 +0000

    Add packages script

---
+++ run_once_before_install-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+sudo apt update
+sudo apt install -y \
+	vim \
+	build-essential
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+/home/linuxbrew/.linuxbrew/bin/brew install \
+	gcc \
+	zsh \
+	dust \
+	bitwarden-cli
+
+
